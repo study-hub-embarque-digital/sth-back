2598b58bfb789cd692d26c52cb807f2b5bae12ee
Clóvis Chakrian <clovischakriandev@gmail.com>
2024-10-07 22:50:08 -0300

    chore: subindo projeto base

---
+++ src/main/java/com/studyhub/sth/SthApplication.java
@@ -0,0 +1,13 @@
+package com.studyhub.sth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyhub/sth/SthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhub.sth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
