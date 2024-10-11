7396255f21d2c199a38e7693845dee22ff5f0c60
AC <acsimoes446@gmail.com>
2024-10-10 22:43:55 -0300

    feat:add método buscar por nome squad

--- src/main/java/com/studyhub/sth/repositories/ISquadRepositorio.java
+++ src/main/java/com/studyhub/sth/repositories/ISquadRepositorio.java
@@ -3,7 +3,11 @@ package com.studyhub.sth.repositories;
 import com.studyhub.sth.entities.Squad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ISquadRepositorio extends JpaRepository<Squad, UUID> {
+    Optional<Squad> findByNome(String nome);
+
 }
+
