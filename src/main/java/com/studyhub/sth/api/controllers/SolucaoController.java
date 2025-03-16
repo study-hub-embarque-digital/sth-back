bb7b2f43d980ea3e02f754aea6e92a64547b6c45
GustavoFelips <gustavofbs1234@gmail.com>
2025-03-16 20:22:16 -0300

    Feat: Crud basico Solucao

--- src/main/java/com/studyhub/sth/api/controllers/SolucaoController.java
+++ src/main/java/com/studyhub/sth/api/controllers/SolucaoController.java
@@ -1,5 +1,10 @@
 package com.studyhub.sth.api.controllers;
+import com.studyhub.sth.application.dtos.duvida.DuvidaDto;
+import com.studyhub.sth.application.dtos.duvida.NewDuvidaDto;
+import com.studyhub.sth.application.dtos.duvida.UpdateDuvidaDto;
+import com.studyhub.sth.application.dtos.solucao.CreateSolucaoDto;
 import com.studyhub.sth.application.dtos.solucao.SolucaoDto;
+import com.studyhub.sth.application.dtos.solucao.UpdateSolucaoDto;
 import com.studyhub.sth.domain.services.ISolucaoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +27,24 @@ public class SolucaoController {
     public ResponseEntity<List<SolucaoDto>> getAll() {
         return ResponseEntity.ok(this.solucaoService.findAll());
     }
-    
+
+    @PostMapping
+    public ResponseEntity<SolucaoDto> create(@RequestBody CreateSolucaoDto solucaoDto) {
+        return new ResponseEntity<>(this.solucaoService.create(solucaoDto), HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SolucaoDto> update(@PathVariable UUID id, @RequestBody UpdateSolucaoDto updateDuvidaDto) {
+        SolucaoDto dto = solucaoService.update(updateDuvidaDto, id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        solucaoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
