00dd5ff84e6b8ab20055bfe0e6fcf9a5c6b18021
GustavoFelips <gustavofbs1234@gmail.com>
2025-03-16 20:22:16 -0300

    Feat: Crud basico Solucao

---
+++ src/main/java/com/studyhub/sth/application/dtos/solucao/CreateSolucaoDto.java
@@ -0,0 +1,30 @@
+package com.studyhub.sth.application.dtos.solucao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import com.studyhub.sth.application.dtos.tag.TagDto;
+import com.studyhub.sth.domain.entities.Duvida;
+import com.studyhub.sth.domain.entities.Usuario;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateSolucaoDto {
+    private String descricao;
+    private UUID duvida;
+    private UUID usuario;
+
+}
