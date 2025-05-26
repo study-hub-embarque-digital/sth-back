c0d7db6a14e75eba6283fe47114a49a32d2c21a4
GustavoFelips <gustavofbs1234@gmail.com>
2025-05-25 21:06:18 -0300

    feat: resolver duvida

--- src/main/java/com/studyhub/sth/domain/repositories/ISolucaoRepository.java
+++ src/main/java/com/studyhub/sth/domain/repositories/ISolucaoRepository.java
@@ -1,11 +1,14 @@
 package com.studyhub.sth.domain.repositories;
 import com.studyhub.sth.domain.entities.Solucao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
 
 public interface ISolucaoRepository extends JpaRepository<Solucao, UUID> {
+    @Query(value = "SELECT COUNT(*) FROM solucoes WHERE duvida_id = :duvidaId", nativeQuery = true)
+    long contarSolucoesPorDuvida(@Param("duvidaId") UUID duvidaId);
 }
-
